c53d4627a352b175ba2cee431ccc4c908b1acb38
Umut Topalak <umuttopalak@hotmail.com>
2023-05-24 09:02:40 +0300

    language files added

---
+++ languages/en.py
@@ -0,0 +1,10 @@
+translations = {
+    'label_text': 'Enter the music link then click the download button.',
+    'button_text': 'Download',
+    'destination' : 'Downloads',
+    'exists' : 'File already exists.',
+    'success_message' : 'has been successfully downloaded.',
+    'VideoUnavailable' : 'Video is Unavailable',
+    'RegexMatchError' : 'Invalid video URL',
+    'Exception' : 'an unexpected error occurred'
+}
---
+++ languages/tr.py
@@ -0,0 +1,10 @@
+translations = {
+    'label_text': 'Müzik linkini giriniz ve indir butonuna tıklayınız.',
+    'button_text': 'İndir',
+    'destination' : 'İndirilenler',
+    'exists' : 'Dosya zaten mevcut',
+    'success_message' : 'başarıyla indirildi',
+    'VideoUnavailable' : 'Video bulunamadı',
+    'RegexMatchError' : 'Geçersiz video urlsi',
+    'Exception' : "Beklenmeyen bir hata oluştu"
+}
